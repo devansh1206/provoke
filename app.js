7be0da0e3686a8fd3ea0b8ebd60d6b3a188bcaf0
devansh1206 <devanshchauhan08@gmail.com>
2023-12-16 12:50:30 +0530

    final

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const API_KEY = 'AIzaSyBoiy_w45CX-DtvDbz1tfsE4t7SiSn9nBM';
 const VIDEO_ID = 'YcCtOaMf8wo';
 
 let player;
-
+// var vid = getElementByName("videoID")
 function onYouTubeIframeAPIReady() {
+    const vid = document.getElementById("videoID").value;
     player = new YT.Player('player', {
         height: '360',
         width: '640',
-        videoId: VIDEO_ID,
+        videoId: vid,
         playerVars: {
             'playsinline': 1,
         },
