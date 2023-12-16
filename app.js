016decd20d35bd1b30a8ab88dc5954f677f1083d
devansh1206 <devanshchauhan08@gmail.com>
2023-12-16 10:41:46 +0530

    final

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-// Replace YOUR_API_KEY with your actual YouTube API key
 const API_KEY = 'AIzaSyBoiy_w45CX-DtvDbz1tfsE4t7SiSn9nBM';
-
-// Replace YOUR_VIDEO_ID with the unlisted YouTube video ID
 const VIDEO_ID = 'YcCtOaMf8wo';
 
 let player;
@@ -26,8 +23,6 @@ function onPlayerReady(event) {
 }
 
 function onPlayerStateChange(event) {
-    // You can implement additional logic based on player state changes
-    // For example, pause the video when it ends
     if (event.data == YT.PlayerState.ENDED) {
         player.pauseVideo();
     }
